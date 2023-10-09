ebaf427c9fe14e58abb67a4b29853038cfa31570
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-10-09 19:36:29 -0400

    created user and part models

--- server/models/Part.js
+++ server/models/Part.js
@@ -1,1 +1,59 @@
 const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Part extends Model {}
+
+Part.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        commodity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        mfg: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        mpn: {
+            type: DataTypes.STRING
+        },
+        ipn: {
+            type: DataTypes.STRING
+        },
+        model: {
+            type: DataTypes.STRING
+        },
+        assetTag: {
+            type: DataTypes.STRING,
+        },
+        createEchotag: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        echotag: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        serial: {
+            type: DataTypes.STRING
+        },
+        parentEchotag: {
+            type: DataTypes.STRING
+        },
+        createdBy: {
+            type: DataTypes.INTEGER
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        tableName: 'parts'
+    }
+)
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,43 @@
 const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false            
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
