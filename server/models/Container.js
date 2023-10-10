67bc885ff975c734af679e710a70e7b550da217c
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-10-09 20:09:44 -0400

    created container model

--- server/models/Container.js
+++ server/models/Container.js
@@ -1,1 +1,22 @@
 const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Container extends Model {}
+
+Container.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'container'
+    }
+)
