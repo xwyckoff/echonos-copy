4b616c8046cfc8cfeff3bb5e602c84df49da9796
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-10-16 21:32:54 -0400

    completed login and logout routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -30,8 +30,46 @@ router.post('/', async(req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
+        const user = await User.findOne({where: { username: req.body.username }});
 
-    } catch (err) {
+        if(!user) {
+            res.status(400).json({message: 'Incorrect email or password.'});
+            return;
+        }
+
+        const validPassword = User.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password.' });
+            return;
+        }
         
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.firstName = userData.firstName;
+            req.session.lastName = userData.lastName;
+            req.session.logged_in = true;
+
+            res.json('Logged in successfully.');
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', async (req, res) => {
+    try {
+        if(req.session.logged_in) {
+            req.session.destroy(() => {
+                res.status(204).end();
+            })
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(400).json(err); 
     }
 })
+
+module.exports(router);
